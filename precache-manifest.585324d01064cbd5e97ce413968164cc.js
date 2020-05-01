e801fca0d46c49f3e99107cf6bb2a3cde187f993
korntewin <korntewin.b@gmail.com>
2020-05-01 15:22:56 +0700

    Updates

---
+++ precache-manifest.585324d01064cbd5e97ce413968164cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b745f9ec8c7b5ce31dc8ba21de5e34",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "12b25320b3118960dd14",
+    "url": "/monster/static/css/main.579ab024.chunk.css"
+  },
+  {
+    "revision": "684a63fdcfa1f79a6337",
+    "url": "/monster/static/js/2.3ac13d0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.3ac13d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12b25320b3118960dd14",
+    "url": "/monster/static/js/main.6cb95178.chunk.js"
+  },
+  {
+    "revision": "5d50faacb9d4340078bb",
+    "url": "/monster/static/js/runtime-main.17d55189.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster/static/media/logo.5d5d9eef.svg"
+  }
+]);
